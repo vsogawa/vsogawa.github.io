0b9eb3be4578a2fc6f789be853a5686b19eca944
vsogawa <36315323+vsogawa@users.noreply.github.com>
2018-03-09 21:02:42 -0800

    Add files via upload

---
+++ js/designs.js
@@ -0,0 +1,18 @@
+let color = $("#colorpicker").val();
+
+
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera    
+}
+
+$("#backToTop").on("mousedown", topFunction);
+
+
+$("#colorpicker").change(function(){
+	color = $("#colorpicker").val();
+	$(".colorChange").css("color", color);
+	$("hr").css("border", `1px solid ${color}`);
+	$("li").css("color", color);	
+	$(".divider").css("background", `linear-gradient(to right, #D6D6D6, #D6D6D6, ${color})`)
+})
